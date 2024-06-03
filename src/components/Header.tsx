82f3992e829099a0d003cee0839964b38bbaee76
Godwin Emeribe <godwinemeribe23@gmail.com>
2024-06-03 09:31:49 +0100

    Started working on my personal portfolio

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className="flex justify-between py-4">
+            <div id="logo-placeholder">O</div>
+            <div id="menu-hamburger">-</div>
+        </header>
+    );
+};
+
+export default Header;
