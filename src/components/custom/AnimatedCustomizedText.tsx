885170c25da3cbe0b4f36794123a0d53ebe61eb1
Godwin Emeribe <godwinemeribe23@gmail.com>
2024-06-13 23:28:25 +0100

    Changed the previous animation to a better one
    The previous animation was buggy so I removed it and replaced it with something better.
    The idea came to me while cooking. Lol.

--- src/components/custom/AnimatedCustomizedText.tsx
+++ src/components/custom/AnimatedCustomizedText.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
-import "./AnimatedText.css";
 
 function AnimatedCustomizedText() {
     const [currentPhrase, setCurrentPhrase] = useState(0);
+    const [opacityValue, setOpacityValue] = useState(1);
     const Phrases = [
         "Godwin Emeribe",
         "a frontend developer",
@@ -18,12 +18,23 @@ function AnimatedCustomizedText() {
         return () => clearInterval(IntervalId);
     }, [currentPhrase]);
 
+    useEffect(() => {
+        if (opacityValue === 0) setOpacityValue(() => 1);
+        let intervalId = setInterval(() => {
+            setOpacityValue(() => opacityValue - 0.25);
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, [opacityValue]);
+
     return (
         <div className="container w-[100%]">
-            <p className="text text-3xl text-red-500">
+            <p
+                className={`text text-3xl text-red-500`}
+                style={{ opacity: `${opacityValue}` }}
+            >
                 I am {Phrases[currentPhrase]}
             </p>
-            {/* <p className="text">I am someone</p> */}
         </div>
     );
 }
